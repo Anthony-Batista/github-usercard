2dfeeb620ec8c4fd6134217e8baa796d0a9013ff
anthony-batista <a.batista0321@gmail.com>
2021-03-18 23:31:52 -0400

    mvp

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
@@ -8,7 +10,6 @@
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
     data in order to use it to build your component function
-
     Skip to STEP 3.
 */
 
@@ -17,23 +18,53 @@
     and append the returned markup to the DOM as a child of .cards
 */
 
+const cards = document.querySelector('.cards')
+function receiveDataFromGithub(url) {
+  axios.get(url).then((res) => {
+    const info = res.data
+    
+    console.log(info)
+    
+    const eachInfo = maker(info)
+
+    cards.append(eachInfo)
+  })
+}
+
+receiveDataFromGithub('https://api.github.com/users/anthony-batista')
+
+console.log(cards)
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
     follow this link in your browser https://api.github.com/users/<Your github name>/followers,
     manually find some other users' github handles, or use the list found at the
     bottom of the page. Get at least 5 different Github usernames and add them as
     Individual strings to the friendsArray below.
-
+    List of LS Instructors Github username's:
+      tetondan
+      dustinmyers
+      justsml
+      luishrd
+      bigknell
     Using that array, iterate over it, requesting data for each user, creating a new card for each
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const friendsArray = [
+  'https://api.github.com/users/tetondan',
+  'https://api.github.com/users/dustinmyers',
+  'https://api.github.com/users/justsml',
+  'https://api.github.com/users/luishrd',
+  'https://api.github.com/users/bigknell',
+]
+
+for (let i = 0; i < friendsArray.length; i++) {
+  receiveDataFromGithub(friendsArray[i])
+}
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
-
     <div class="card">
       <img src={image url of user} />
       <div class="card-info">
@@ -49,6 +80,49 @@ const followersArray = [];
       </div>
     </div>
 */
+function maker(userInfo) {
+  const card = document.createElement('div')
+  const image = document.createElement('img')
+  const cardInfo = document.createElement('div')
+  const name = document.createElement('h3')
+  const username = document.createElement('p')
+  const location = document.createElement('p')
+  const profile = document.createElement('p')
+  const profileATag = document.createElement('a')
+  const followers = document.createElement('p')
+  const following = document.createElement('p')
+  const bio = document.createElement('p')
+  
+  card.classList.add('card')
+  cardInfo.classList.add('card-info')
+  name.classList.add('name')
+  username.classList.add('username')
+
+  card.appendChild(image)
+  card.appendChild(cardInfo)
+  cardInfo.appendChild(name)
+  cardInfo.appendChild(username)
+  cardInfo.appendChild(location)
+  cardInfo.appendChild(profile)
+  profile.appendChild(profileATag)
+  cardInfo.appendChild(followers)
+  cardInfo.appendChild(following)
+  cardInfo.appendChild(bio)
+
+  image.src = userInfo.avatar_url
+
+  name.textContent = userInfo.name
+  username.textContent = userInfo.login
+  location.textContent = 'Location:  ' + userInfo.location
+  profileATag.textContent = 'Profile:  ' + userInfo.html_url
+  followers.textContent = 'Followers:  ' + userInfo.followers
+  following.textContent = 'Following:' + userInfo.following
+  bio.textContent = 'Bio:  ' + userInfo.bio
+
+  
+  return card
+  
+}
 
 /*
   List of LS Instructors Github username's:
